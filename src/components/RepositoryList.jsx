081dd70f7076afd5dba7fcfdecb0d7ddd3fa5cd7
Guilherme Tavares <guigo211103@gmail.com>
2022-03-23 16:10:15 -0300

    C1 aula 3-2

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: "unform",
@@ -8,6 +11,12 @@ const repository = {
 };
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json()).then(data => setRepositories(data));
+    }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios: </h1>
